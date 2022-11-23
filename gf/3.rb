770f7b6285329c12f9964326a2f31edaf2d08ade
gronasmall <vladgrona@gmail.com>
2022-11-23 11:59:30 +0200

    123

--- gf/3.rb
+++
@@ -1,8 +0,0 @@
-price=''
-puts "Введіть ціну цукерок для 1 кг"
-price=gets.strip.to_f
-5.times do
-    puts "Введіть в кг вагу цукерок, ціну яких Ви хочете дізнатися"
-    mass=gets.strip.to_f
-    puts price*mass
-end
